f02661b8b246ce649992cefe82d7594cdaaa2bc5
Abdulbaki ÖZER <ozerbaki@hotmail.com.tr>
2022-06-09 02:54:44 +0300

    POST İle Değer döndürme işlemi
    GET ve POST arasındaki farkı tarayıcımızın adres çubuğuna bakarak daha net anlayabiliriz.

--- HTML-Form_Islemleri/index.php
+++ HTML-Form_Islemleri/index.php
@@ -4,6 +4,8 @@
         <title>HTML + PHP</title>
     </head>
     <body>
+        <!-- 
+            //GET İle Değer döndürme işlemi//
         <form actioon="" method="GET">
             <input type="text" name="ad" placeholder="Adınızı girin"> <br>
             <input type="text" name="soyad" placeholder="Soyadınızı girin"> <br>
@@ -31,6 +33,69 @@
                 echo "Tıklanmadı";
             }
         ?>
+        -->
+
+        <!--POST İle Değer döndürme işlemi-->
+        <form actioon="" method="POST">
+            <input type="text" name="ad" placeholder="Adınızı girin"> <br>
+            <input type="text" name="soyad" placeholder="Soyadınızı girin"> <br>
+            Erkek <input type="radio" name="cinsiyet" value="erkek"> <br>
+            Kadın <input type="radio" name="cinsiyet" value="kadin"> <br>
+            <input type="email" name="email" placeholder="example@email.com"> <br>
+            
+            <select name="gun">
+                <?php for ($i=1; $i <=31 ; $i++) 
+                { 
+                    echo '<option value="'.$i.'"> '.$i.' </option>';
+                }
+                ?>
+            </select>
+            <?php
+                $aylar = array("Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık");
+            ?>
+            <select name="ay">
+                <?php 
+                    foreach ($aylar as $ay) 
+                    {
+                        echo '<option value="'.$ay.'">'.$ay.'</option>';
+                    }
+                ?>
+                
+            </select>
+            <select name="yil">
+                <?php for ($i=1900; $i <=date("Y") ; $i++) 
+                { 
+                    echo '<option value="'.$i.'"> '.$i.' </option>';
+                }
+                ?>
+            </select><br><br>
+
+            <input type="submit" name="kaydet" value="Kaydet">
+            <input type="reset" name="temizle" value="Temizle">
+        </form>
+
+        <?php
+            //Eğer kaydet kılandıysa
+            if(@$_POST["kaydet"])
+            {
+                //print_r($_POST);
+                echo "
+                Ad: <b>$_POST[ad]</b><br>
+                Soyad: <b>$_POST[soyad]</b><br>
+                Cinsiyet: <b>$_POST[cinsiyet]</b><br>
+                Email: <b>$_POST[email]</b><br>
+                Doğum: <b>$_POST[gun]/$_POST[ay]/$_POST[yil]</b><br>
+                ";
+            }
+            else 
+            {
+                echo "Tıklanmadı";
+            }
+        ?>
+
+
+
+
 
     </body>
 </html>
