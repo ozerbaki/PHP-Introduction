d6c1e968d077afdf45efd95774b90b28d415f174
Abdulbaki ÖZER <ozerbaki@hotmail.com.tr>
2022-06-09 02:17:53 +0300

    HTML Form tasarımı yapıldı.

---
+++ HTML-Form_Islemleri/index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>HTML + PHP</title>
+    </head>
+    <body>
+        <form actioon="" method="POST">
+            <input type="text" name="ad" placeholder="Adınızı girin"> <br>
+            <input type="text" name="soyad" placeholder="Soyadınızı girin"> <br>
+            Erkek <input type="radio" name="cinsiyet" value="erkek"> <br>
+            Kadın <input type="radio" name="cinsiyet" value="kadin"> <br>
+            <input type="email" name="email" placeholder="example@email.com"> <br>
+            <input type="submit" name="kaydet" value="Kaydet">
+            <input type="reset" name="temizle" value="Temizle">
+        </form>
+    </body>
+</html>
