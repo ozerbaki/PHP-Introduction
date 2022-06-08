498c66ea678183cff578440237ee8ea41d049744
Abdulbaki ÖZER <ozerbaki@hotmail.com.tr>
2022-06-09 02:28:47 +0300

    GET ile değer döndürme işlemi

--- HTML-Form_Islemleri/index.php
+++ HTML-Form_Islemleri/index.php
@@ -4,7 +4,7 @@
         <title>HTML + PHP</title>
     </head>
     <body>
-        <form actioon="" method="POST">
+        <form actioon="" method="GET">
             <input type="text" name="ad" placeholder="Adınızı girin"> <br>
             <input type="text" name="soyad" placeholder="Soyadınızı girin"> <br>
             Erkek <input type="radio" name="cinsiyet" value="erkek"> <br>
@@ -13,5 +13,24 @@
             <input type="submit" name="kaydet" value="Kaydet">
             <input type="reset" name="temizle" value="Temizle">
         </form>
+
+        <?php
+            //Eğer kaydet kılandıysa
+            if(@$_GET["kaydet"])
+            {
+                //print_r($_GET);
+                echo "
+                Ad: <b>$_GET[ad]</b><br>
+                Soyad: <b>$_GET[soyad]</b><br>
+                Cinsiyet: <b>$_GET[cinsiyet]</b><br>
+                Email: <b>$_GET[email]</b><br>
+                ";
+            }
+            else 
+            {
+                echo "Tıklanmadı";
+            }
+        ?>
+
     </body>
 </html>
